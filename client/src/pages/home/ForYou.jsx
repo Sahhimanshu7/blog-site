99744d3958d06a1a9aef357b22baf91989411b18
Himanshu Sah <himanshusah41@gmail.com>
2023-12-17 00:51:30 -0500

    Backend started

--- client/src/pages/home/ForYou.jsx
+++ client/src/pages/home/ForYou.jsx
@@ -1,3 +1,24 @@
+import styled from "styled-components"
+import { BlogDescBox } from "../../components/misc/BlogDescBox"
+
 export const ForYou = () =>{
-    return <p>For You</p>
+    return (
+        <Main>
+            <Header>For You</Header>
+            <BlogDescBox />
+        </Main>
+    )
 }
+
+const Header = styled.div`
+    font-size: 24px;
+    font-weight: 700;
+    margin: 10px;
+`;
+
+const Main = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+`;
--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
 const express = require('express');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+const { createServer } = require("node:http");
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+const httpServer = createServer(app);
+
+dotenv.config();
+
+//mongoose connection establishment
+mongoose.connect(process.env.ATLAS_URI, {useNewUrlParser:true, useUnifiedTOpology:true}). 
+then(()=>{
+    console.log("Connected to database");
+}).catch((e)=>{
+    console.log("An error occurred while connecting to database \n", e);
+})
+
+//middleware
+app.use(express.json());
